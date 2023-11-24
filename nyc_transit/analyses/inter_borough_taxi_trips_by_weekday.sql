0ecfaecf9f168fc164a511923d78045dad086680
jpupp <jpuppala@andrew.cmu.edu>
2023-11-23 20:25:01 -0500

    week 4 sol

--- nyc_transit/analyses/inter_borough_taxi_trips_by_weekday.sql
+++ nyc_transit/analyses/inter_borough_taxi_trips_by_weekday.sql
@@ -1,7 +1,7 @@
 WITH TripData AS ( SELECT WEEKDAY(pickup_datetime) AS weekday, CASE WHEN pu_loc.Borough != do_loc.Borough THEN 1 ELSE 0 END AS is_diff_borough
 FROM {{ ref('mart__fact_all_taxi_trips')}} AS all_taxi_trips
-JOIN {{ ref('mart__dim_locations')}} AS pu_loc ON CAST(all_taxi_trips.PUlocationID AS INT) = pu_loc.LocationID
-JOIN {{ ref('mart__dim_locations')}} AS do_loc ON CAST(all_taxi_trips.DOlocationID AS INT) = do_loc.LocationID
+JOIN {{ ref('mart__dim_locations')}} AS pu_loc ON all_taxi_trips.PUlocationID = pu_loc.LocationID
+JOIN {{ ref('mart__dim_locations')}} AS do_loc ON all_taxi_trips.DOlocationID = do_loc.LocationID
 )
 SELECT weekday,COUNT(*) AS total_trips,SUM(is_diff_borough) AS diff_borough_trips,(SUM(is_diff_borough) * 100.0 / COUNT(*)) AS percent_diff_borough
 FROM TripData
--- nyc_transit/analyses/taxi_trips_ending_at_airport.sql
+++ nyc_transit/analyses/taxi_trips_ending_at_airport.sql
@@ -1,4 +1,4 @@
 SELECT count(*) AS airport_trips
 FROM  {{ ref('mart__fact_all_taxi_trips')}} AS all_taxi_trips
-JOIN {{ ref('mart__dim_locations')}} AS loc ON CAST(all_taxi_trips.DOlocationID AS INT) = loc.LocationID
+JOIN {{ ref('mart__dim_locations')}} AS loc ON all_taxi_trips.DOlocationID = loc.LocationID
 WHERE loc.service_zone IN ('Airports', 'EWR')
